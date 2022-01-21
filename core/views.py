241264bce4d0a3328446ddc2e60f574b45d206c3
gil-cesar-martins <gilcesartst@gmail.com>
2022-01-21 12:07:47 -0300

    Salvando dados e midia no database

--- core/views.py
+++ core/views.py
@@ -28,15 +28,12 @@ def contato(request):
 def produto(request):
     if str(request.method) == 'POST':
         form = ProdutoModelForm(request.POST, request.FILES)
-        if form.is_valid():
-            prod = form.save(commit=False)
+        if form.is_valid():  
 
-            print(f'Nome: {prod.nome}')
-            print(f'Preço: {prod.preco}')
-            print(f'Estoque: {prod.estoque}')
-            print(f'Imagem: {prod.imagem}')
-
+            form.save()
+            
             messages.success(request, 'Produto salvo com sucesso.')
+            form = ProdutoModelForm()
         else:
             messages.error(request, 'Erro ao salvar produto.')
     else:
